24f3630879d132167c8d6bc4630bc56f980b54ca
Maggie Benson <Magdalene.Benson@hgst.com>
2016-03-14 14:06:02 -0500

    do conversions

---
+++ client/helpers/_helpers.js
@@ -0,0 +1,17 @@
+Helpers = {
+
+    prettyBinary: function (binary) {
+
+        // prepend more zeros to make the binary
+        // number display is pretty chunks of 4 ints
+        while (binary.length % 4 !== 0) {
+            binary = "0" + binary;
+        }
+        return binary.replace(/(.{4})/g, '$1 ').trim();
+    },
+    cleanBinary: function(binary) {
+        // Strip the whiteplaces
+        return binary.replace(" ", "").trim();
+    }
+
+};
--- client/helpers/home.js
+++ client/helpers/home.js
@@ -3,35 +3,6 @@ Template.home.rendered = function() {
     $(".loading").hide();
 };
 
-Template.home.events({
-    "change .base-select": function(e,t) {
-
-        var newBase = $(".base-select").val();
-        Session.set("base", newBase);
-    },
-    "input .iban": function(e,t) {
-        // Only allow digits 1 and 0 and add a space after every four characters
-        e.target.value = e.target.value.replace(/[^\(0-1)]/g, '').replace(/(.{4})/g, '$1 ').trim();
-    },
-    "input .hexi": function(e,t) {
-        // Only allow characters A-F
-        e.target.value = e.target.value.replace(/[^\d(A-F)(a-f)]/g, '').trim();
-    },
-    "input .deci": function(e,t) {
-        // Only allow digits
-        e.target.value = e.target.value.replace(/[^\(0-9)]/g, '');
-    },
-    "click #process": function(e,t) {
-
-        e.preventDefault();
-        $(".loading").show();
-
-        setTimeout(function() {
-            $(".loading").hide();
-        }, 3000);
-    }
-});
-
 Template.home.helpers({
     basePlaceholder: function() {
 
@@ -50,4 +21,75 @@ Template.home.helpers({
                 break;
         }
     }
-})
+});
+
+Template.home.events({
+    "change .base-select": function(e,t) {
+
+        var newBase = $(".base-select").val();
+        Session.set("base", newBase);
+        $("input").val("");
+    },
+    "input .iban": function(e,t) {
+        // Only allow digits 1 and 0 and add a space after every four characters
+        e.target.value = e.target.value.replace(/[^\(0-1)]/g, '').replace(/(.{4})/g, '$1 ').trim();
+    },
+    "input .hexi": function(e,t) {
+        // Only allow characters A-F
+        e.target.value = e.target.value.replace(/[^\d(A-F)(a-f)]/g, '').trim();
+    },
+    "input .deci": function(e,t) {
+        // Only allow digits
+        e.target.value = e.target.value.replace(/[^\(0-9)]/g, '');
+    },
+    "click #process": function(e,t) {
+
+        e.preventDefault();
+        $(".loading").show();
+
+        var base = Session.get("base");
+        var currValue = $("input").val();
+        console.log("Value: " + currValue + " in " + base);
+
+        var result = {};
+
+        if (base == "binary") {
+            currValue = Helpers.cleanBinary(currValue);
+        }
+        // ELSE omitted intentionally
+
+        switch (base) {
+            case "hex":
+
+                result.binary = parseInt(currValue, 16).toString(2);
+                result.octal = parseInt(currValue, 16).toString(8).toUpperCase();
+                result.decimal = parseInt(currValue, 16).toString(10);
+                result.hex = currValue.toUpperCase();
+                break;
+
+            case "binary":
+
+                result.binary = currValue;
+                result.octal = parseInt(currValue, 2).toString(8).toUpperCase();
+                result.decimal = parseInt(currValue, 2).toString(10);
+                result.hex = parseInt(currValue, 2).toString(16).toUpperCase();
+                break;
+
+            case "decimal":
+            default:
+
+                result.binary = parseInt(currValue, 10).toString(2);
+                result.octal = parseInt(currValue, 10).toString(8).toUpperCase();
+                result.decimal = currValue;
+                result.hex = parseInt(currValue, 10).toString(16).toUpperCase();
+                break;
+        }
+
+        console.log("Binary: " + Helpers.prettyBinary(result.binary));
+        console.log("Octal: " + result.octal);
+        console.log("Decimal: " + result.decimal);
+        console.log("Hex: " + result.hex);
+
+        $(".loading").hide();
+    }
+});
